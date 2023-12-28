759208d4cc33ed381cce66d6d6175df7cff3704f
Brandon Gormley <brandontgormley@gmail.com>
2023-12-28 13:28:11 -0600

    create & add Hero component

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+    return (
+        <section className='w-full max-w-[1440px] mx-auto px-8'>
+            <p>
+                <span className='text-slate-400 text-2xl font-extrabold text-center'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Esse, itaque?
+                </span>
+            </p>
+        </section>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Hero from './components/Hero';
+
 export default function Home() {
     return (
         <main>
-            <h1>Welcome to Nova</h1>
-            <p>A personal blog website to write our my project ideas</p>
+            <Hero />
         </main>
     );
 }
