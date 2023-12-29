5a0001a749025be3d00f75d1fc18b99890898ee9
Brandon Gormley <brandontgormley@gmail.com>
2023-12-29 10:09:11 -0600

    update styles & content to hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,12 +1,23 @@
+import Link from 'next/link';
+
 export default function Hero() {
     return (
-        <section className='w-full max-w-[1440px] mx-auto px-8'>
-            <p>
-                <span className='text-slate-400 text-2xl font-extrabold text-center'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Esse, itaque?
-                </span>
-            </p>
+        <section className='w-full mx-auto px-8 container mt-32'>
+            <div className='lg:py-16 '>
+                <h1 className='text-3xl font-bold tracking-tighter md:text-4xl lg:text-5xl xl:text-6xl  '>
+                    Welcome to Nova
+                </h1>
+                <p className='max-w-[600px] text-gray-500 md:text-xl py-4 mb-4'>
+                    Explore the world of knowledge and creativity with engaging
+                    articles and stories.
+                </p>
+                <Link
+                    className='bg-black text-white rounded-lg px-12 py-3 block text-center md:inline hover:bg-slate-600 transition-all'
+                    href='/'
+                >
+                    Explore Now
+                </Link>
+            </div>
         </section>
     );
 }
