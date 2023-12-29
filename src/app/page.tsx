54dec5ba48815b4920b131914fe3b5e177d669bb
Brandon Gormley <brandontgormley@gmail.com>
2023-12-29 09:12:11 -0600

    add blogpost component for placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import BlogPost from './components/BlogPost';
 import Hero from './components/Hero';
 
 export default function Home() {
     return (
         <main>
             <Hero />
+            <section className='p-8 flex flex-wrap justify-center mx-auto'>
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+                <BlogPost />
+            </section>
         </main>
     );
 }
