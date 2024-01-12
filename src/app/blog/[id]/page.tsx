7c17eca44e075e3567c99f5678445f32fcad2d59
Brandon Gormley <brandontgormley@gmail.com>
2024-01-12 10:48:24 -0600

    create blog detail page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,41 @@
-export default function BlogPost() {
+interface BlogPostProps {
+    params: {
+        id: string;
+    };
+}
+
+export interface BlogPost {
+    id: number;
+    title: string;
+    body: string;
+    date: string;
+}
+
+async function getData(id: string) {
+    const res = await fetch(`http://localhost:4000/blogposts/${id}`, {
+        next: {
+            revalidate: 0,
+        },
+    });
+    return res.json();
+}
+
+export default async function BlogPost({ params }: BlogPostProps) {
+    const blogpost: BlogPost = await getData(params.id);
     return (
-        <main>
-            <h1>Blog Post Page</h1>
+        <main className='min-h-screen flex-1 py-8 px-6 flex flex-col w-full max-w-[1440px] mx-auto'>
+            <div className='p-6 flex flex-col'>
+                <div className='flex justify-between text-slate-400 text-sm md:text-xs xl:text-sm'>
+                    <p>Blog Post: {blogpost.id}</p>
+                    <p>{blogpost.date}</p>
+                </div>
+                <div className='py-4 text-slate-600'>
+                    <p className='font-bold text-3xl'>{blogpost.title}</p>
+                    <p className='text-base text-slate-500 my-2'>
+                        {blogpost.body}
+                    </p>
+                </div>
+            </div>
         </main>
     );
 }
