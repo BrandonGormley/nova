112f4629493f1406191db48f5b8dffad4274d4ad
Brandon Gormley <brandontgormley@gmail.com>
2024-01-12 10:47:46 -0600

    remove blogs from hp for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,7 @@ export default function Home() {
     return (
         <main className='min-h-screen'>
             <Hero />
-            <section className='p-8 flex flex-wrap justify-center mx-auto max-w-[1440px]'>
-                <BlogPost />
-                <BlogPost />
-                <BlogPost />
-                <BlogPost />
-                <BlogPost />
-            </section>
+            <section className='p-8 flex flex-wrap justify-center mx-auto max-w-[1440px]'></section>
         </main>
     );
 }
