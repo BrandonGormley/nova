48fa2ff1c1471bc7ba4a0b7010d0803dec1c9ae3
Brandon Gormley <brandontgormley@gmail.com>
2024-01-26 16:24:13 -0600

    styling update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <main className='min-h-screen'>
             <section className='w-full mx-auto px-8 max-w-[1440px] mt-32'>
                 <div className='lg:py-16 '>
-                    <h1 className='text-3xl font-bold tracking-tighter md:text-4xl lg:text-5xl xl:text-6xl  '>
+                    <h1 className='text-3xl font-bold tracking-tighter md:text-4xl lg:text-5xl xl:text-6xl text-gray-800'>
                         Welcome to Nova
                     </h1>
                     <p className='max-w-[600px] text-gray-500 md:text-xl py-4 mb-4'>
@@ -13,7 +13,7 @@ export default function Home() {
                         engaging articles and stories.
                     </p>
                     <Link
-                        className='bg-slate-900 text-white rounded-lg px-6 py-3 block text-center md:inline hover:bg-slate-800 transition-all'
+                        className='bg-gray-800 text-white rounded-lg px-6 py-3 block text-center md:inline hover:bg-gray-700 transition-all'
                         href='/blog'
                     >
                         Explore Now
