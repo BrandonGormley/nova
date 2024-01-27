2a841ad1dc8049f830766474beb2eb55c7c8c5e7
Brandon Gormley <brandontgormley@gmail.com>
2024-01-27 14:38:30 -0600

    add suspense & revalidate to blog detail page

--- src/app/(root)/blog/[id]/page.tsx
+++ src/app/(root)/blog/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { notFound } from 'next/navigation';
 import { BlogPost } from '@/app/lib/definitions';
+import { Suspense } from 'react';
+import { LoadingSpinner } from '@/app/components/ui/LoadingSkeletons';
 
 interface BlogPostProps {
     params: {
@@ -11,6 +13,7 @@ export async function generateMetadata({ params }: BlogPostProps) {
     const data: BlogPost = await getData(params.id);
     return {
         title: `Nova | ${data.title}`,
+        description: `${data.body.slice(0, 155)}`,
     };
 }
 
@@ -26,7 +29,7 @@ export async function generateStaticParams() {
 async function getData(id: string) {
     const res = await fetch(`http://localhost:4000/blogposts/${id}`, {
         next: {
-            revalidate: 0,
+            revalidate: 60,
         },
     });
 
@@ -40,18 +43,20 @@ export default async function BlogPostDetail({ params }: BlogPostProps) {
     const blogpost: BlogPost = await getData(params.id);
     return (
         <main className='min-h-screen flex-1 py-8 px-6 flex flex-col w-full max-w-[1440px] mx-auto'>
-            <div className='p-6 flex flex-col'>
-                <div className='flex justify-between text-slate-400 text-sm md:text-xs xl:text-sm'>
-                    <p>Blog Post: {blogpost.id}</p>
-                    <p>{blogpost.date}</p>
+            <Suspense fallback={<LoadingSpinner />}>
+                <div className='p-6 flex flex-col'>
+                    <div className='flex justify-between text-slate-400 text-sm md:text-xs xl:text-sm'>
+                        <p>Blog Post: {blogpost.id}</p>
+                        <p>{blogpost.date}</p>
+                    </div>
+                    <div className='py-4 text-slate-600'>
+                        <p className='font-bold text-3xl'>{blogpost.title}</p>
+                        <p className='text-base text-slate-500 my-2'>
+                            {blogpost.body}
+                        </p>
+                    </div>
                 </div>
-                <div className='py-4 text-slate-600'>
-                    <p className='font-bold text-3xl'>{blogpost.title}</p>
-                    <p className='text-base text-slate-500 my-2'>
-                        {blogpost.body}
-                    </p>
-                </div>
-            </div>
+            </Suspense>
         </main>
     );
 }
