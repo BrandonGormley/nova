33496ff491bc6abd2a6ce2aee243ae332c2d68e4
Brandon Gormley <brandontgormley@gmail.com>
2024-02-01 12:49:58 -0600

    add POST request

--- src/app/api/blogposts/route.ts
+++ src/app/api/blogposts/route.ts
@@ -8,3 +8,19 @@ export async function GET(request: Request) {
         status: 200,
     });
 }
+
+export async function POST(request: Request) {
+    const blogpost = await request.json();
+
+    const res = await fetch('http://localhost:4000/blogposts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(blogpost),
+    });
+
+    const newBlogPost = await res.json();
+
+    return NextResponse.json(newBlogPost, {
+        status: 201,
+    });
+}
