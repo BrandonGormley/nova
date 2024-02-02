8e12694211de26e20294b490d9057e97927bbfeb
Brandon Gormley <brandontgormley@gmail.com>
2024-02-02 14:53:10 -0600

    add verify page

---
+++ src/app/(auth)/verify/page.tsx
@@ -0,0 +1,12 @@
+export default function Verify() {
+    return (
+        <main className='flex-1 py-8 px-6 flex flex-col min-h-screen w-full max-w-[1440px] mx-auto'>
+            <h2 className='text-2xl font-bold mb-8 w-full mx-auto text-gray-800 text-center'>
+                Thanks for registerring
+            </h2>
+            <p className='text-center'>
+                Please verify your email address before logging in
+            </p>
+        </main>
+    );
+}
