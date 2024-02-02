01ac46b0fbf44c0227d02830b3ca24c381cc4b9e
Brandon Gormley <brandontgormley@gmail.com>
2024-02-02 14:14:29 -0600

    update login & signup routes with content

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
+import LoginForm from '@/app/components/ui/LoginForm';
+
 export default function LoginPage() {
     return (
         <main className='flex-1 py-8 px-6 flex flex-col min-h-screen w-full max-w-[1440px] mx-auto'>
-            <h2 className='text-2xl font-bold mb-8 w-full mx-auto text-gray-800'>
-                Login Page
+            <h2 className='text-2xl font-bold mb-8 w-full mx-auto text-gray-800 text-center'>
+                Login
             </h2>
+            <LoginForm />
         </main>
     );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,12 @@
+import SignUpForm from '@/app/components/ui/SignUpForm';
+
 export default function SignupPage() {
     return (
         <main className='flex-1 py-8 px-6 flex flex-col min-h-screen w-full max-w-[1440px] mx-auto'>
-            <h2 className='text-2xl font-bold mb-8 w-full mx-auto text-gray-800'>
-                Signup Page
+            <h2 className='text-2xl font-bold mb-8 w-full mx-auto text-gray-800 text-center'>
+                Sign Up
             </h2>
+            <SignUpForm />
         </main>
     );
 }
