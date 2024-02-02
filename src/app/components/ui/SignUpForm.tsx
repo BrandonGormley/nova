938f947081f7dd2c1b78d621d8bade6ccae721b5
Brandon Gormley <brandontgormley@gmail.com>
2024-02-02 14:14:18 -0600

    create signup form component

---
+++ src/app/components/ui/SignUpForm.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { FormEvent, useState } from 'react';
+
+export default function SignUpForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (
+        e: FormEvent,
+        email: string,
+        password: string
+    ) => {
+        e.preventDefault();
+
+        console.log(`Signup: ${email} ${password}`);
+    };
+
+    return (
+        <form
+            onSubmit={(e) => handleSubmit(e, email, password)}
+            className='w-full lg:w-1/3 mx-auto flex flex-col bg-gray-100 rounded-lg p-8'
+        >
+            <label className='flex flex-col text-gray-600'>
+                <span className='my-2'>Email:</span>
+                <input
+                    className='rounded py-1 outline-gray-400 border-none pl-2'
+                    type='text'
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label className='flex flex-col text-gray-600'>
+                <span className='my-2'>Password:</span>
+                <input
+                    className='rounded py-1 outline-gray-400 border-none pl-2'
+                    type='text'
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className='bg-secondary text-white rounded-md text-center px-4 py-2 w-full mx-auto lg:max-w-[33%] my-4'>
+                Sign Up
+            </button>
+        </form>
+    );
+}
