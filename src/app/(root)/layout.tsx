6fd3e5a23673a8f50e94a2b2f8d586aeeb788409
Brandon Gormley <brandontgormley@gmail.com>
2024-02-03 13:50:57 -0600

    add logic to get user data

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,19 @@
 import Header from '@/app/components/Header';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const supabase = createServerComponentClient({ cookies });
+    const { data } = await supabase.auth.getSession();
+
     return (
         <>
-            <Header />
+            <Header user={data.session?.user} />
             {children}
         </>
     );
