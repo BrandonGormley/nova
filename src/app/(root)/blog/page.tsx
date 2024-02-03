64b6f0e875961c335603a43412f4698c2fe33c08
Brandon Gormley <brandontgormley@gmail.com>
2024-02-03 13:49:00 -0600

    add an add button that links to create

--- src/app/(root)/blog/page.tsx
+++ src/app/(root)/blog/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import Loading from '@/app/(root)/blog/loading';
 import BlogPostList from '@/app/components/ui/BlogPostList';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
     title: 'Nova | Blog',
@@ -10,9 +11,17 @@ export const metadata: Metadata = {
 export default function Blog() {
     return (
         <main className='flex-1 py-8 px-6 flex flex-col min-h-screen w-full max-w-[1440px] mx-auto'>
-            <h2 className='text-2xl font-bold mb-8 w-full mx-auto text-gray-800'>
-                All Articles
-            </h2>
+            <div className='flex justify-start items-center mb-4'>
+                <h2 className='text-2xl font-bold text-gray-800 ml-4'>
+                    All Articles
+                </h2>
+                <Link
+                    className='bg-secondary hover:bg-secondary-light transition-all text-white rounded-md text-xs px-2 py-1 text-center ml-4'
+                    href='/blog/create'
+                >
+                    Add +
+                </Link>
+            </div>
             <section className='flex flex-wrap w-full'>
                 <Suspense fallback={<Loading />}>
                     <BlogPostList />
