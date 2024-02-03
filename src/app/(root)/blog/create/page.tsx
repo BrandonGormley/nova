2bdcb01f3b836d02b4766c080f50c2dbe71c4123
Brandon Gormley <brandontgormley@gmail.com>
2024-02-03 13:54:58 -0600

    add route protection on create page so only logged in users can create

--- src/app/(root)/blog/create/page.tsx
+++ src/app/(root)/blog/create/page.tsx
@@ -1,6 +1,15 @@
 import CreateBlogPostForm from '@/app/components/ui/CreateBlogPostForm';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function CreateBlogPage() {
+export default async function CreateBlogPage() {
+    const supabase = createServerComponentClient({ cookies });
+    const { data } = await supabase.auth.getSession();
+
+    if (!data.session) {
+        redirect('/login');
+    }
     return (
         <main className='flex-1 py-8 px-6 flex flex-col min-h-screen w-full max-w-[1440px] mx-auto'>
             <h2 className='text-2xl font-bold mb-8 w-full mx-auto text-gray-800 text-center'>
