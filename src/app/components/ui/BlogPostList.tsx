8e8a0eda5ef50889649ef504091cd789327c869e
Brandon Gormley <brandontgormley@gmail.com>
2024-02-05 11:30:22 -0600

    rewrite logic to hit db rather than api endpoint

--- src/app/components/ui/BlogPostList.tsx
+++ src/app/components/ui/BlogPostList.tsx
@@ -1,28 +1,34 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import BlogPostCard from './BlogPostCard';
 import { BlogPost } from '@/app/lib/definitions';
+import { cookies } from 'next/headers';
 
 async function getData() {
-    const res = await fetch(`http://localhost:4000/blogposts`, {
-        next: {
-            revalidate: 60,
-        },
-    });
-    return res.json();
+    const supabase = createServerComponentClient({ cookies });
+
+    const { data, error } = await supabase.from('blogposts').select();
+
+    if (error) {
+        console.log(error.message);
+    }
+
+    return data;
 }
 
 export default async function BlogPostList() {
-    const blogposts: BlogPost[] = await getData();
+    const blogposts: BlogPost[] | null = await getData();
     return (
         <ul className='flex flex-wrap w-full justify-center'>
-            {blogposts.map((blogpost) => (
-                <li
-                    className='border border-slate-100 shadow-md rounded-md m-4 w-full md:max-w-[40%] lg:max-w-[25%]'
-                    key={blogpost.id}
-                >
-                    <BlogPostCard {...blogpost} />
-                </li>
-            ))}
-            {blogposts.length === 0 && <p>No blog posts available</p>}
+            {blogposts &&
+                blogposts.map((blogpost) => (
+                    <li
+                        className='border border-slate-100 shadow-md rounded-md m-4 w-full md:max-w-[40%] lg:max-w-[25%]'
+                        key={blogpost.id}
+                    >
+                        <BlogPostCard {...blogpost} />
+                    </li>
+                ))}
+            {!blogposts && <p>No blog posts available</p>}
         </ul>
     );
 }
