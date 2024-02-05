6ed009378f62cb8027c391f38871244cb49ebfe2
Brandon Gormley <brandontgormley@gmail.com>
2024-02-05 12:28:34 -0600

    add metadata base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { rubik } from '@/app/lib/fonts';
 export const dynamic = 'force-dynamic';
 
 export const metadata: Metadata = {
+    metadataBase: new URL('https://nova-bg.vercel.app/'),
     title: 'Welcome to Nova',
     description:
         'Welcome to Nova, a captivating space where words come alive. Explore insightful articles, personal stories, and engaging content that reflect my journey and passions. Join me on this immersive blog as we delve into topics ranging from lifestyle, travel, tech, and more. Lets embark on a journey of discovery together at Nova.',
