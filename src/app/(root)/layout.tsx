7f187cfad945f815be52989182dd8d3d0c26f131
Brandon Gormley <brandontgormley@gmail.com>
2024-02-07 09:58:40 -0600

    remove pointless import

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,6 @@
 import Header from '@/app/components/Header';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export default async function RootLayout({
     children,
