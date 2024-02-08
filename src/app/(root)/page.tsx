894ff25d042f4ef305f71f9c0a9bf596abd8aa1d
Brandon Gormley <brandontgormley@gmail.com>
2024-02-08 08:13:29 -0600

    add blogpostlist to hp

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import BlogPostList from '../components/ui/blog/BlogPostList';
 
 export default function Home() {
     return (
         <main className='min-h-screen'>
             <section className='w-full mx-auto px-8 max-w-[1440px] mt-32'>
-                <div className='lg:py-16 '>
+                <div className='lg:py-16'>
                     <h1 className='text-3xl font-bold tracking-tighter md:text-4xl lg:text-5xl xl:text-6xl text-gray-800'>
                         Welcome to Nova
                     </h1>
@@ -19,6 +20,7 @@ export default function Home() {
                         Explore Now
                     </Link>
                 </div>
+                <BlogPostList />
             </section>
         </main>
     );
